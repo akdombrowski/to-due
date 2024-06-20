e0280428444c54bb67767c630297fa140d070b68
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-06-20 09:15:34 -0500

    gitignore updates. removed unused style files from create next app template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Box from "@mui/material/Box"
+import Grid from "@mui/material/Unstable_Grid2"
+import Container from "@mui/material/Container"
 
 export default function Home() {
   return (
